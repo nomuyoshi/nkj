77f81f7807ca9eb1e69d837cbf689a07463fcee9
nomuyoshi <nomuyoshi0111@gmail.com>
2024-03-09 05:40:57 +0900

    descriminate jisx0213 chars

--- lib/nkj.rb
+++ lib/nkj.rb
@@ -1,8 +1,45 @@
 # frozen_string_literal: true
 
-require_relative "nkj/version"
+require 'csv'
+require_relative 'nkj/version'
 
-module Nkj
+module NKJ
   class Error < StandardError; end
-  # Your code goes here...
+  class << self
+    def jisx0213?(str)
+      encode(str).each_grapheme_cluster do |char|
+        cp = 'U+' + char.codepoints.map { |p| format('%04X', p) }.join('+')
+        return false if mapping[cp].nil?
+      end
+
+      true
+    end
+
+    private
+
+    def mapping
+      @mapping ||= load_mapping
+    end
+
+    def load_mapping
+      tsv = CSV.read('./source/jisx0213-2004-8bit-std.txt', col_sep: "\t", skip_lines: /\A##/)
+      mapping = {}
+      tsv.each do |row|
+        unicode_point = row[1]
+        next if unicode_point.nil?
+
+        # unicode_point = Integer(row[1].gsub('U+', '0x'))
+        jis_code_point = Integer(row[0])
+        mapping[unicode_point] = jis_code_point
+      end
+
+      mapping
+    end
+
+    def encode(str)
+      return str if str.encoding == Encoding::UTF_8
+
+      str.encode(Encoding::UTF_8)
+    end
+  end
 end
